65a84f7154442f186fb1c1a89376132805d96048
FroggyKwa <ilya.laguxa@gmail.com>
2019-10-26 00:58:45 +1000

    Cosmetic editions

--- ChatterBox/main.py
+++ ChatterBox/main.py
@@ -1,16 +1,14 @@
-from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication
+from ui.MainForm import *
 import sys
 
 
-class ChatterBox(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('../ui/MainForm.ui', self)
-        self.show()
 
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = ChatterBox()
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec_())
