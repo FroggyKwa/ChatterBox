17e1f63dd9672f2cef7342bdf65e1122d054c50a
froggy <ilya.laguxa@gmail.com>
2019-10-27 12:16:15 +1000

    twisted server

--- ChatterBox/server.py
+++ ChatterBox/server.py
@@ -1,51 +1,49 @@
-import socket
-import select
-
-IP = 'localhost'
-PORT = 1234
-
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-server_socket.bind((IP, PORT))
-server_socket.listen()
-
-sockets_list = [server_socket]
-clients = {}
+from twisted.internet.error import ConnectionDone
+from twisted.internet.protocol import ServerFactory
+from twisted.internet import reactor
+from twisted.protocols.basic import LineOnlyReceiver
 
 
-def receive_message(client_socket):
-    try:
-        message_header = client_socket.recv(10)
-        if not len(message_header):
-            return False
-        message_length = int(message_header.decode('utf-8').strip())
-        return {"header": message_header, 'data': client_socket.recv(message_length)}
-    except Exception:
-        return False
+class Handler(LineOnlyReceiver):
+    factory: 'Server'
 
+    def connectionMade(self):
+        self.factory.clients.append(self)
+        print(f'connected {self}')
+        self.login: str = None
 
-while True:
-    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+    def connectionLost(self, reason=ConnectionDone):
+        self.factory.clients.remove(self)
+        print('disconnected')
 
-    for notified_socket in read_sockets:
-        if notified_socket == server_socket:
-            client_socket, client_address = server_socket.accept()
-            user = receive_message(client_socket)
-            if user is False:
-                sockets_list.append(client_socket)
-            clients[client_socket] = user
-            print(f'Accepted new connection from {client_address[0]}:{client_address[1]}\tusername {user["data"].decode("utf-8")}')
+    def lineReceived(self, line):
+        message = line.decode()
+        if self.login is not None:
+            message = f'<{self.login}>: {message}'
+            for user in self.factory.clients:
+                if user is not self:
+                    user.sendLine(message.encode())
         else:
-            message = receive_message(notified_socket)
-            if message is False:
-                print(f'Closed connection from {clients[notified_socket]["data"].decode("utf-8")}')
-                sockets_list.remove(notified_socket)
-                del clients[notified_socket]
-                continue
-            user = clients[notified_socket]
-            print(f'Received message from {user["data"].decode("utf-8")}: {message["data"].decode("utf-8")}')
+            if message.startswith('/login '):
+                login = message.replace('/login', '')
+                self.login = login
+                print(f'new user -> {login}')
+                self.sendLine(f'Welcome, {login}'.encode())
+            else:
+                self.sendLine('login incorrect'.encode())
 
-            for client_socket in clients:
-                if client_socket != notified_socket:
-                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+            
+
+
+class Server(ServerFactory):
+    def __init__(self):
+        self.clients = list()
+
+    protocol = Handler
+
+    def startFactory(self):
+        print('Server started')
+
+
+reactor.listenTCP(7410, Server())
+reactor.run()
