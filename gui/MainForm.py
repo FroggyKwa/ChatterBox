555bdf42ada16abec2daea2d3fa0882cdf41318b
FroggyKwa <ilya.laguxa@gmail.com>
2019-11-04 15:02:42 +1000

    edit menubar action

--- gui/MainForm.py
+++ gui/MainForm.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QMainWindow, QLabel
-from ui import login, signup, edit
+from gui import login, signup, edit
 
 
 class ChatterBox(QMainWindow):
